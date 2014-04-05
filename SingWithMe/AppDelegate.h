bb2c76fdd791b7dd51bd42d743479b48e1931df4
EzGame <zyq.david@gmail.com>
2014-04-05 13:07:08 -0400

    Initial Commit

---
+++ SingWithMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingWithMe
+//
+//  Created by David Zhang on 2014-04-05.
+//  Copyright (c) 2014 EzGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingWithMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SingWithMe
+//
+//  Created by David Zhang on 2014-04-05.
+//  Copyright (c) 2014 EzGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
